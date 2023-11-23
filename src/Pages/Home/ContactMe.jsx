a3c495f1857b5575e3e56d35e80336b3c58d147d
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-11-24 03:26:03 +0600

    Contact me done

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,23 +1,23 @@
 import { useForm } from "react-hook-form";
-import { FaEnvelope, FaLocationArrow, FaPhoneAlt } from "react-icons/fa";
+import { FaEnvelope, FaEnvelopeOpenText, FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
 const ContactMe = () => {
-  //   const {
-  //     register,
-  //     trigger,
-  //     formState: { errors },
-  //   } = useform();
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
 
-  //   const onSubmit = async (e) => {
-  //     const isValid = await trigger();
-  //     if (!isValid) {
-  //       e.preventDefault();
-  //     }
-  //   };
+  const onSubmit = async (e) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
 
   return (
-    <div className="custom-container text-white ">
+    <div id="Contact" className="custom-container text-white ">
       <div className="text-center text-7xl font-bold text-white relative mb-20 mt-20 uppercase ">
         <span className="border-b-8 border-primaryColor absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-4 w-20  "></span>
         Contact Me
@@ -25,21 +25,21 @@ const ContactMe = () => {
       <div className="bg-secondaryBgColor rounded-3xl  mb-20 text-center ">
         <div className=" flex flex-col lg:flex-row  ">
           <div className="flex-auto  lg:w-2/5  w-full bg-[#2c2c30] rounded-s-3xl  ">
-            <div className="my-10 flex flex-col space-y-14 ">
-              <div className="flex justify-end gap-2 ">
+            <div className=" grid grid-cols-1 gap-4 content-evenly h-full sm:my-10 lg:my-0 ">
+              <div className="flex justify-end gap-2  ">
                 <div className=" text-3xl  font-semibold ">CONTACT</div>
                 <div className="h-1 w-64 my-auto  bg-white"></div>
               </div>
 
               {/* Contact Details Start */}
-              <div className="flex flex-col gap-4">
-                <div className="flex gap-1 items-center justify-start ps-20">
+              <div className="flex flex-col gap-4 text-lg sm:ps-36 lg:ps-24 sm:my-10 lg:my-0 ">
+                <div className="flex gap-1 items-center justify-start">
                   <FaPhoneAlt />: +8801789502830
                 </div>
-                <div className="flex gap-1 items-center justify-start ps-20">
+                <div className="flex gap-1 items-center justify-start">
                   <FaEnvelope />: abidhasan00123@gmail.com
                 </div>
-                <div className="flex gap-1 items-center justify-start ps-20">
+                <div className="flex gap-1 items-center justify-start">
                   <FaLocationDot />: Dhaka, Bangladesh
                 </div>
               </div>
@@ -47,8 +47,110 @@ const ContactMe = () => {
               <div className="h-1 w-64 bg-white "></div>
             </div>
           </div>
-          <div className="flex-auto lg:w-3/5 w-full ">
-            <div className="mt-20">form</div>
+          {/* Form Section */}
+          <div className="flex-auto lg:w-3/5 w-full  ">
+            <div className="mt-10">
+              <div>
+                <form
+                  target="_blank"
+                  onSubmit={onSubmit}
+                  action="https://formsubmit.co/36b3a266b39aed34c10c4824d22cb3ca"
+                  method="POST"
+                >
+                  <div className=" flex w-11/12 justify-between mx-auto mt-10 ">
+                    <div className="w-5/12 ">
+                      <input
+                        className="input w-full bg-[#2c2c30]"
+                        type="text"
+                        placeholder="NAME"
+                        {...register("name", {
+                          required: true,
+                          maxLength: 100,
+                        })}
+                      />
+                      <p className="text-red-500 mt-1 ">
+                        {errors &&
+                          errors.name &&
+                          errors.name.type === "required" &&
+                          "Please enter your name"}
+                        {errors &&
+                          errors.name &&
+                          errors.name.type === "maxLength" &&
+                          "Max length is 100 char."}
+                      </p>
+                    </div>
+
+                    <div className="w-6/12">
+                      <input
+                        className="input w-full bg-[#2c2c30]"
+                        type="email"
+                        placeholder="EMAIL"
+                        {...register("email", {
+                          required: true,
+                          pattern:
+                            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                        })}
+                      />
+                      <p className="text-red-500 mt-1 ">
+                        {errors &&
+                          errors.email &&
+                          errors.email.type === "required" &&
+                          "Email is required"}
+                        {errors &&
+                          errors.email &&
+                          errors.email.type === "pattern" &&
+                          "Invalid email address."}
+                      </p>
+                    </div>
+                  </div>
+
+                  <input
+                    className="input w-11/12 mt-10 bg-[#2c2c30] "
+                    type="text"
+                    placeholder="SUBJECT"
+                    {...register("subject", {
+                      required: false,
+                      maxLength: 200,
+                    })}
+                  />
+                  <p className="text-red-500 mt-1 ">
+                    {errors &&
+                      errors.subject &&
+                      errors.subject.type === "maxLength" &&
+                      "Max length is 200 char."}
+                  </p>
+
+                  <textarea
+                    className="textarea textarea-bordered textarea-lg w-11/12  mt-10 bg-[#2c2c30]"
+                    type="text"
+                    placeholder="MESSAGE"
+                    {...register("message", {
+                      required: true,
+                      maxLength: 2000,
+                    })}
+                  />
+                  <p className="text-red-500 mt-1 ">
+                    {errors &&
+                      errors.message &&
+                      errors.message.type === "required" &&
+                      "Message is required"}
+                    {errors &&
+                      errors.message &&
+                      errors.message.type === "maxLength" &&
+                      "Max length is 2000 char."}
+                  </p>
+                  <div className="flex justify-end w-11/12 mx-auto ">
+                    <button
+                      type="submit"
+                      className="btn hover:bg-hoverColor active:bg-activeColor bg-primaryColor text-black items-center text-lg text-center font-semibold px-10  rounded-xl transition-colors duration-300 mb-10 mt-9 "
+                    >
+                      <FaEnvelopeOpenText />
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
